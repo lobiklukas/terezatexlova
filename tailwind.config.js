8bb7b1bbdd7503b3cb508e70972033c66e759c4c
Lukáš Lobík <lobiklukas@gmail.com>
2020-08-28 01:10:49 +0200

    add width to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   purge: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
-  theme: {},
+  theme: {
+    minWidth: {
+      "0": "0",
+      "1/4": "25%",
+      "1/2": "50%",
+      "3/4": "75%",
+      full: "100%",
+    },
+  },
   variants: {},
   plugins: [],
 }
