424bf3801ad58c51af9808ddc35076352880c262
Lukáš Lobík <lobiklukas@gmail.com>
2020-08-28 01:37:43 +0200

    add github pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 
 module.exports = {
+  pathPrefix: `./terezatexlova`,
   siteMetadata: {
     title: `Tereza Texlová`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
